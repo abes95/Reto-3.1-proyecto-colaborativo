ce73c84d9d6973325f3bff53bc995ccca235b9ae
Donovan Saucedo <donovioac@hotmail.com>
2026-01-30 13:15:12 +0100

    Cambio 2: Añadida información sobre el CRUD en la vista de bienvenida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,7 +53,7 @@
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 bg-white dark:bg-[#161615] dark:text-[#EDEDEC] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none">
                     <h1 class="mb-1 font-medium">Proyecto Colaborativo: Donovan y Jorge</h1>
-                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">MOdificado para el Reto 3.1 <br>We suggest starting with the following.</p>
+                    <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Modificado para el Reto 3.1 <br>We suggest starting with the following.</p>
                     <ul class="flex flex-col mb-4 lg:mb-6">
                         <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:top-1/2 before:bottom-0 before:left-[0.4rem] before:absolute">
                             <span class="relative py-1 bg-white dark:bg-[#161615]">
@@ -109,6 +109,20 @@
                                 </a>
                             </span>
                         </li>
+			<li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border:-[#3E3E3A] before:bottom-1/2 before:top-0 before:left-[0.4rem] before:absolute">
+				<span class="relative py-1 bg-white dark:bg-[#161615]">
+					<span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border dark:border-[#3E3E3A] border-[#e3e3e0]">
+						<span class="rounded-full bg-[#f53003] w-1.5 h-1.5"></span>
+					</span>
+				</span>
+				<span>
+					Explora la funcionalidad
+					<span class="font-medium text-[#f53003] dark:text-[#FF4433] ml-1">
+						CRUD completo
+					</span>
+					desarrollado por Donovan.
+				</span>
+			</li>
                     </ul>
                     <ul class="flex gap-3 text-sm leading-normal">
                         <li>
